75f2eb9e62ad4911a8c3a14b2cc2e567634cf7c5
Lauren Grant <lgrant1286@gmail.com>
2016-06-12 23:38:59 -0400

    Adds functionality to sidebar.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
+// Sidebar
+
 $('.ui.labeled.icon.sidebar').first()
   .sidebar('attach events', '.toggle.button')
 ;
+
 $('.toggle.button')
   .removeClass('disabled')
 ;
+
+$(function() {
+  $("#about-me, #about-nola, #contact-me").on("click", function( e ) {
+      e.preventDefault();
+      $("body, html").animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+      }, 200);
+  });
+});
+
+// Modal
 
 $('.contact').on('click', function(){
   $('.ui.modal')
